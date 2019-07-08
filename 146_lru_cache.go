5902bdc231ebb68cf31ea1052e44f92dd93c3211
frankie <elitegoblinrb@gmail.com>
2019-07-08 15:32:48 +1000

    MON

---
+++ 146_lru_cache.go
@@ -0,0 +1,49 @@
+package amazon
+
+import "container/list"
+
+type LRUCache struct {
+	mp       map[int]*list.Element
+	list     *list.List
+	capacity int
+}
+
+func Constructor(capacity int) LRUCache {
+	return LRUCache{
+		mp:       make(map[int]*list.Element),
+		list:     list.New(),
+		capacity: capacity,
+	}
+}
+
+type element struct {
+	key   int
+	value int
+}
+
+func (this *LRUCache) Get(key int) int {
+	if e, ok := this.mp[key]; ok {
+		this.list.MoveToFront(e)
+		return e.Value.(*element).value
+	}
+	return -1
+}
+
+func (this *LRUCache) Put(key int, value int) {
+	if e, ok := this.mp[key]; ok {
+		this.list.MoveToFront(e)
+		e.Value.(*element).value = value
+		return
+	}
+	if this.list.Len() >= this.capacity {
+		tail := this.list.Back()
+		key := tail.Value.(*element).key
+		delete(this.mp, key)
+		this.list.Remove(tail)
+	}
+	this.list.PushFront(&element{
+		key:   key,
+		value: value,
+	})
+	this.mp[key] = this.list.Front()
+}
---
+++ common.go
@@ -0,0 +1,6 @@
+package amazon
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
