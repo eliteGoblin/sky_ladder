af0ada9653f64c07b8f7e9305a918ddd2de4671a
Frank Sun <frank.sun@nearmap.com>
2019-07-16 08:54:46 +1000

    trap

--- common.go
+++ common.go
@@ -4,3 +4,23 @@ type ListNode struct {
 	Val  int
 	Next *ListNode
 }
+
+func max(a... int)int {
+	m := a[0]
+	for i := 1; i < len(a); i ++ {
+		if a[i] > m {
+			m = a[i]
+		}
+	}
+	return m
+}
+
+func min(a... int)int {
+	m := a[0]
+	for i := 1; i < len(a); i ++ {
+		if a[i] < m {
+			m = a[i]
+		}
+	}
+	return m
+}
