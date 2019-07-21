a277f5d4f284010309d13691f933aaca52c4e62e
frankie <elitegoblinrb@gmail.com>
2019-07-21 12:39:01 +1000

    sun

--- common.go
+++ common.go
@@ -15,3 +15,13 @@ func test() {
 	sort.Sort(sort.Reverse(sort.IntSlice(s)))
 	fmt.Println(s)
 }
+
+func max(a ...int) int {
+	res := a[0]
+	for i := 1; i < len(a); i++ {
+		if a[i] > res {
+			res = a[i]
+		}
+	}
+	return res
+}
