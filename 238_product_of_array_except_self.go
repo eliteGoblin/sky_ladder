155611a8e97e7982b6db57afcee1b9b6d970467c
Frank Sun <frank.sun@nearmap.com>
2019-08-19 08:33:43 +1000

    mon

--- 238_product_of_array_except_self.go
+++ 238_product_of_array_except_self.go
@@ -7,7 +7,7 @@ func productExceptSelf(nums []int) []int {
 	}
 	res[0] = 1
 	for i := 1; i < len(nums); i++ {
-		res[i] *= res[i-1]
+		res[i] = nums[i-1] * res[i-1]
 	}
 	product := 1
 	for i := len(nums) - 1; i >= 0; i-- {
